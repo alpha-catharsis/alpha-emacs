c48a61ce301b0f461e3a4e0c2655be94f28b4752
Alpha Catharsis <alpha.catharsis@gmail.com>
2021-08-17 17:31:42 +0200

    Added avy-flycheck

---
+++ src/alpha/system-packages/install-avy-flycheck.el
@@ -0,0 +1,5 @@
+;;; install-avy-flycheck.el -*- lexical-binding: t; -*-
+
+(straight-use-package 'avy-flycheck)
+
+;;; install-avy-flycheck.el ends here
---
+++ src/alpha/system-packages/load-avy-flycheck.el
@@ -0,0 +1,6 @@
+;;; load-avy-flycheck.el -*- lexical-binding: t; -*-
+
+(use-package avy-flycheck
+  :bind (("C-c n E" . avy-flycheck-goto-error)))
+
+;;; load-avy-flycheck.el ends here
