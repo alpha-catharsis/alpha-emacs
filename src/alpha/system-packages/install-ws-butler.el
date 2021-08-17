4844e295c7dfb6027a2c04d5cee3eb8492647856
Alpha Catharsis <alpha.catharsis@gmail.com>
2021-08-17 16:16:52 +0200

    Added ws-butler

---
+++ src/alpha/system-packages/install-ws-butler.el
@@ -0,0 +1,5 @@
+;;; install-ws-butler.el -*- lexical-binding: t; -*-
+
+(straight-use-package 'ws-butler)
+
+;;; install-ws-butler.el ends here
---
+++ src/alpha/system-packages/load-ws-butler.el
@@ -0,0 +1,8 @@
+;;; load-ws-butler.el -*- lexical-binding: t; -*-
+
+(use-package ws-butler
+  :config
+  (setq ws-butler-keep-whitespace-before-point nil)
+  (ws-butler-global-mode t))
+
+;;; load-ws-butler.el ends here
