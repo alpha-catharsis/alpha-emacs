51d49cc6c6b2ff31bc06f7a0ecb69749d9af64ec
Alpha Catharsis <alpha.catharsis@gmail.com>
2021-08-17 16:26:46 +0200

    Added rainbow-delimiters

---
+++ src/alpha/system-packages/install-rainbow-delimiters.el
@@ -0,0 +1,5 @@
+;;; install-rainbow-delimiters.el -*- lexical-binding: t; -*-
+
+(straight-use-package 'rainbow-delimiters)
+
+;;; install-rainbow-delimiters.el ends here
---
+++ src/alpha/system-packages/load-rainbow-delimiters.el
@@ -0,0 +1,7 @@
+;;; load-rainbow-delimiters.el -*- lexical-binding: t; -*-
+
+(use-package rainbow-delimiters
+  :config
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+
+;;; load-rainbow-delimiters.el ends here
